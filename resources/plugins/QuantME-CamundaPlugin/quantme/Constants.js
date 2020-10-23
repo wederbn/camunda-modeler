9d9a3807c6743bbe3ddf67833c1adf7279fa4636
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-10-23 14:58:48 +0200

    Use constants for QuantME QNames

---
+++ resources/plugins/QuantME-CamundaPlugin/quantme/Constants.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2020 Institute for the Architecture of Application System -
+ * University of Stuttgart
+ *
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// QNames of the QuantME task types
+export const QUANTUM_COMPUTATION_TASK = 'quantme:QuantumComputationTask';
+export const QUANTUM_CIRCUIT_LOADING_TASK = 'quantme:QuantumCircuitLoadingTask';
+export const DATA_PREPARATION_TASK = 'quantme:DataPreparationTask';
+export const ORACLE_EXPANSION_TASK = 'quantme:OracleExpansionTask';
+export const QUANTUM_CIRCUIT_EXECUTION_TASK = 'quantme:QuantumCircuitExecutionTask';
+export const READOUT_ERROR_MITIGATION_TASK = 'quantme:ReadoutErrorMitigationTask';
