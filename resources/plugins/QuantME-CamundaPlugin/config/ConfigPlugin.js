59733935517b75bdcbb44b27b513bf47ea045486
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2021-02-09 15:47:33 +0100

    Add modal for the modeler configuration

--- resources/plugins/QuantME-CamundaPlugin/config/ConfigPlugin.js
+++ resources/plugins/QuantME-CamundaPlugin/config/ConfigPlugin.js
@@ -13,21 +13,54 @@
 import React, { Fragment, PureComponent } from 'camunda-modeler-plugin-helpers/react';
 import { Fill } from 'camunda-modeler-plugin-helpers/components';
 
+import ConfigModal from './ConfigModal';
+
+const defaultState = {
+  configOpen: false
+};
+
 export default class ConfigPlugin extends PureComponent {
+
   constructor(props) {
     super(props);
+
+    this.state = defaultState;
+
+    this.handleConfigClosed = this.handleConfigClosed.bind(this);
+  }
+
+  handleConfigClosed(newConfig) {
+    this.setState({ configOpen: false });
+
+    if (newConfig) {
+
+      // TODO
+      console.log('Received new config...');
+      console.log(newConfig);
+    }
   }
 
   render() {
 
     // render config button and pop-up menu
     return (<Fragment>
-      <Fill slot="toolbar" group="9_autoSave">
+      <Fill slot="toolbar">
         <button type="button" className="src-app-primitives-Button__Button--3Ffn0" title="Open configuration menu"
-          onClick={ () => this.setState({ configOpen: true }) } >
+          onClick={() => this.setState({ configOpen: true })}>
           <span className="app-icon-properties"><span className="indent">Configuration</span></span>
         </button>
       </Fill>
+      {this.state.configOpen && (
+        <ConfigModal
+          onClose={this.handleConfigClosed}
+          initValues={{
+            camundaEndpoint: 'TODO',
+            opentoscaEndpoint: 'TODO',
+            qrmRepoName: 'TODO',
+            qrmUserName: 'TODO'
+          }}
+        />
+      )}
     </Fragment>);
   }
 }
